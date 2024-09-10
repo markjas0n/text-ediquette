1b32cb86def80dddf335413e246bb683e95ddbac
markjas0n@icloud.com <markjas0n@icloud.com>
2024-09-09 22:58:40 -0500

    unresolved errors when npm run build after consolidating package.json

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,63 +1,61 @@
-// text-ediquette/Develop/client/webpack.config.js
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE',
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js', // The source service worker file
-        swDest: 'service-worker.js', // The destination in the dist folder
-      }),
-      new WebpackPwaManifest({
-        name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'A simple text editor application',
-        background_color: '#ffffff',
-        theme_color: '#31a9e1',
-        start_url: './',
-        publicPath: './',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
-    ],
-    module: {
-      rules: [
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'Develop/client/index.html',
+      title: 'JATE',
+    }),
+    new InjectManifest({
+      swSrc: 'Develop/client/src-sw.js', // Ensure this file exists in the correct location
+      swDest: 'service-worker.js',
+    }),
+    new WebpackPwaManifest({
+      name: 'Just Another Text Editor',
+      short_name: 'JATE',
+      description: 'A simple text editor application',
+      background_color: '#ffffff',
+      theme_color: '#31a9e1',
+      start_url: './',
+      publicPath: './',
+      icons: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
+          src: path.resolve('Develop/client/src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
         },
       ],
-    },
-  };
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
+console.log('Resolved image path:', path.resolve(__dirname, 'src/images/logo.png'));
